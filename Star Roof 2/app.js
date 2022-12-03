881d1e76577dde9e2febf6e61c1c04baa545e3f5
leonardo2203 <113115406+leonardo2203@users.noreply.github.com>
2022-12-03 12:39:14 -0500

    actualizacion 2

---
+++ Star Roof 2/app.js
@@ -0,0 +1,26 @@
+import express from "express"
+import mongoose from "mongoose";
+import categoryRouter from "./router/categoryRouter.js";
+
+ 
+const app=express();
+
+const port =process.env.PORT || 8080
+app.listen(port, ()=>{
+   console.log("El servidor se esta ejecuntado correctamente ");
+})
+
+mongoose.connect("mongodb+srv://clientes:leonardo22034@clusterclientes.rk2pe43.mongodb.net///database-cliente?retryWrites=true&w=majority",
+(err)=>{
+
+   if (err) {
+   console.log(err);
+}else{
+   console.log("conectado a la base de datos.");
+
+}
+})
+
+
+app.use(express.json())
+app.use("/category",categoryRouter)
