0e116c020c26fa9848cf545287ee11eb2df629f0
NORMAN TOSSE <norman.tosse.mt@usa.edu.co>
2022-12-12 10:46:23 -0500

    Delete Star Roof 2 directory

--- Star Roof 2/Model/clientModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const clientModel = mongoose.Schema({
-  nombre: { type: String, required: true },
-  identificacion: { type: String, required: true },
-  correo: { type: String, required: true },
-  edad: { type: Number, required: true },
-});
-
-export default mongoose.model("clientes", clientModel);
--- Star Roof 2/Model/userAdminModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const userAdminModel = mongoose.Schema({
-  nombre: { type: String, required: true },
-  correo: { type: String, required: true },
-  contrasena: { type: String, required: true },
-});
-
-export default mongoose.model("administrators", userAdminModel);
