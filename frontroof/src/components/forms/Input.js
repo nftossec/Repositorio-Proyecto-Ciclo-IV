f0da779fe1a2a806532aebca3721ad4493b926ad
normanfelipe <norman.tosse.mt@usa.edu.co>
2022-12-18 14:18:00 -0500

    elimino carpeta front roof para actualizar sprint 3

--- frontroof/src/components/forms/Input.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import "./forms.css"
-
-export default function Input(props) {
-    
-  const {children,type} = props
-
-  return(<input className= 'input' type={type} placeholder={children}></input>)
-}
--- frontroof/src/views/Home.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-
-function Home() {
-
-  return <h1>BIENVENIDO A STAR ROOF</h1>;
-}
-
-export default Home;
