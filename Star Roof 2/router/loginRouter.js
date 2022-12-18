8c2af9b95b8ef235bb266fe6058825521ec4d255
normanfelipe <norman.tosse.mt@usa.edu.co>
2022-12-18 14:17:09 -0500

    elimino carpeta star roof para actualizar sprint 3

--- Star Roof 2/router/loginRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import login from "../controller/loginController.js";
-
-const loginRouter = express.Router();
-
-loginRouter.get("/", login);
-
-export default loginRouter;
