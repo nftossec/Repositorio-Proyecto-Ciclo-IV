0ef0540fa2de0bff9b93586989c206b5199a961e
normanfelipe <norman.tosse.mt@usa.edu.co>
2022-12-23 05:54:10 -0500

    Se elimina proyecto star roof sprint 3 al ser innecesario

--- Sprint 3/Star Roof 2/Model/userAdminModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose"
-
-const userAdminModel = mongoose.Schema({
-    "nombreUserAdmin":{type:String, required:true, maxLength:50, unique: true},
-    "correo":{type:String, required:true, maxLength:50, unique:true},
-    "contrasena":{type:String, required:true, unique:true}
-})
-
-export default mongoose.model("administrators", userAdminModel)
--- Sprint 3/Star Roof 2/router/registerRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { register } from "../controller/registerController.js";
-
-const registerRouter = express.Router();
-
-registerRouter.post("/", register);
-
-export default registerRouter;
